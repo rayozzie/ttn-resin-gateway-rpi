894007eac2346c222e550e4e63ff1b0bc9005fd3
Ray Ozzie <ray@ozzie.net>
2016-03-21 15:58:37 -0700

    asdf

--- start.sh
+++ start.sh
@@ -1,8 +1,22 @@
 #! /bin/bash
 
+# Display the environment, which is useful when debugging
+
+echo "**************************************************************"
+echo "**************************************************************"
+echo "Environment:"
+env
+echo "**************************************************************"
+echo "**************************************************************"
+echo ""
+
 # Configure the global and local configuration files via the environment
 
-echo "Configuring gateway:"
+echo "**************************************************************"
+echo "**************************************************************"
+echo "Gateway Configuration:"
+echo "**************************************************************"
+echo "**************************************************************"
 
 if [[ $GATEWAY_NAME == "" ]]; then
     echo "ERROR: NO GATEWAY_NAME FOUND IN ENVIRONMENT"
@@ -34,12 +48,23 @@ if [[ $GATEWAY_ALT == "" ]]; then
 fi
 echo GATEWAY_ALT: $GATEWAY_ALT
 
+echo "**************************************************************"
+echo "**************************************************************"
 echo ""
 
 echo -e "{\n\t\"gateway_conf\": {\n\t\t\"gateway_ID\": \"0000000000000000\",\n\t\t\"servers\": [ { \"server_address\": \"croft.thethings.girovito.nl\", \"serv_port_up\": 1700, \"serv_port_down\": 1701, \"serv_enabled\": true } ],\n\t\t\"ref_latitude\": $GATEWAY_LAT,\n\t\t\"ref_longitude\": $GATEWAY_LON,\n\t\t\"ref_altitude\": $GATEWAY_ALT,\n\t\t\"contact_email\": \"$GATEWAY_EMAIL\",\n\t\t\"description\": \"$GATEWAY_NAME\" \n\t}\n}" >./local_conf.json
 
 # Reset gateway ID based on MAC
 ../packet_forwarder/reset_pkt_fwd.sh start ./local_conf.json
+
+# Display gateway ID, which is very important for debugging
+
+echo "**************************************************************"
+echo "**************************************************************"
+grep gateway_ID local_conf.json
+echo "**************************************************************"
+echo "**************************************************************"
+echo ""
 
 # Test the connection, wait if needed.
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
@@ -52,6 +77,10 @@ while true
   do
     echo "[TTN Gateway]: Starting packet forwarder..."
     ./poly_pkt_fwd
+	echo "**************************************************************"
+	echo "**************************************************************"
     echo "[TTN Gateway]: Packet forwarder exited; retrying in 15s..."
+	echo "**************************************************************"
+	echo "**************************************************************"
     sleep 15
   done
--- start.sh~
+++ start.sh~
@@ -36,10 +36,10 @@ echo GATEWAY_ALT: $GATEWAY_ALT
 
 echo ""
 
-echo -e "{\n\t\"gateway_conf\": {\n\t\t\"gateway_ID\": \"0000000000000000\",\n\t\t\"servers\": [ { \"server_address\": \"croft.thethings.girovito.nl\", \"serv_port_up\": 1700, \"serv_port_down\": 1701, \"serv_enabled\": true } ],\n\t\t\"ref_latitude\": $GATEWAY_LAT,\n\t\t\"ref_longitude\": $GATEWAY_LON,\n\t\t\"ref_altitude\": $GATEWAY_ALT,\n\t\t\"contact_email\": \"$GATEWAY_EMAIL\",\n\t\t\"description\": \"$GATEWAY_NAME\" \n\t}\n}" >./bin/local_conf.json
+echo -e "{\n\t\"gateway_conf\": {\n\t\t\"gateway_ID\": \"0000000000000000\",\n\t\t\"servers\": [ { \"server_address\": \"croft.thethings.girovito.nl\", \"serv_port_up\": 1700, \"serv_port_down\": 1701, \"serv_enabled\": true } ],\n\t\t\"ref_latitude\": $GATEWAY_LAT,\n\t\t\"ref_longitude\": $GATEWAY_LON,\n\t\t\"ref_altitude\": $GATEWAY_ALT,\n\t\t\"contact_email\": \"$GATEWAY_EMAIL\",\n\t\t\"description\": \"$GATEWAY_NAME\" \n\t}\n}" >./local_conf.json
 
 # Reset gateway ID based on MAC
-./packet_forwarder/reset_pkt_fwd.sh start ./bin/local_conf.json
+../packet_forwarder/reset_pkt_fwd.sh start ./local_conf.json
 
 # Test the connection, wait if needed.
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
