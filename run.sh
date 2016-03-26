a8510160fb41a68fd2b9be771341b0223280ba79
Ray Ozzie <ray@ozzie.net>
2016-03-26 15:18:15 -0400

    build fixes

--- run.sh
+++ run.sh
@@ -150,6 +150,16 @@ echo "******************"
 echo "******************"
 echo ""
 
+# Reset iC880a PIN - this is necessary to reset its state
+
+gpio -1 mode 22 out
+gpio -1 write 22 0
+sleep 0.1
+gpio -1 write 22 1
+sleep 0.1
+gpio -1 write 22 0
+sleep 0.1
+
 # Test the connection, wait if needed.
 
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
