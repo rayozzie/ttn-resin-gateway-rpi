d1bfb656967b7b48d0143d756b3a48f988fcd62d
Ray Ozzie <ray@ozzie.net>
2016-04-10 16:09:51 -0400

    had to apt-get install iproute

--- run.sh
+++ run.sh
@@ -149,6 +149,7 @@ echo -e "{\n\
 # Set gateway_ID in local_conf.json to the gateway's MAC address
 
 echo "******************"
+apt-get install --only-upgrade iproute
 ./set-gateway-id.sh start ./local_conf.json
 echo "******************"
 echo ""
