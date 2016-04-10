7c7e5be4ca24c3e097bb87eec0b59fcdaaf5d85c
Ray Ozzie <ray@ozzie.net>
2016-04-10 17:28:39 -0400

    debug

--- run.sh
+++ run.sh
@@ -91,7 +91,7 @@ if [[ $GW_FORWARD_CRC_VALID == "" ]]; then GW_FORWARD_CRC_VALID="true"; fi
 if [[ $GW_FORWARD_CRC_ERROR == "" ]]; then GW_FORWARD_CRC_ERROR="false"; fi
 if [[ $GW_FORWARD_CRC_DISABLED == "" ]]; then GW_FORWARD_CRC_DISABLED="false"; fi
 
-if [[ $GW_GPS_TTY_PATH == "" ]]; then GW_GPS_TTY_PATH="\"/dev/ttyS0\""; fi
+if [[ $GW_GPS_TTY_PATH == "" ]]; then GW_GPS_TTY_PATH="\"/dev/ttyAMA0\""; fi
 if [[ $GW_FAKE_GPS == "" ]]; then GW_FAKE_GPS="false"; fi
 
 if [[ $GW_GHOST_ADDRESS == "" ]]; then GW_GHOST_ADDRESS="\"127.0.0.1\""; fi
